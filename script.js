a5defeb5d38289d355120365478ce39f066098b1
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-04-21 02:21:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('snippets').innerHTML = `
+  <div class="snippet">
+    <pre>console.log("Hello Open Source");</pre>
+  </div>
+`;
+
+document.getElementById('leaderboard').innerHTML = `
+  <li>Rishi - 1 contribution</li>
+`;
