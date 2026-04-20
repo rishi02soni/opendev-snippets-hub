0dddfce916c44c30d95d53498e4ea9b31d9b50ae
Rishi Soni <167216843+rishi02soni@users.noreply.github.com>
2026-04-21 02:41:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,25 @@
-// fetch('/data/snippets.json')
-//   .then(res => res.json())
-//   .then(snippets => {
-//     snippets.forEach(snippet => {
-//       document.getElementById('snippets').innerHTML += `
-//         <div class="snippet">
-//           <h3>${snippet.title}</h3>
-//           <pre>${snippet.code}</pre>
-//         </div>
-//       `;
-//     });
-//   });
-
-// // leaderboard
-// fetch('/data/contributors.json')
-//   .then(res => res.json())
-//   .then(data => {
-//     let sorted = data.sort((a, b) => b.contributions - a.contributions);
-
-//     sorted.forEach(user => {
-//       document.getElementById('leaderboard').innerHTML += `
-//         <li>${user.name} - ${user.contributions}</li>
-//       `;
-//     });
-//   });
 fetch('/data/snippets.json')
   .then(res => res.json())
   .then(snippets => {
-    snippets.forEach((snippet, index) => {
+    snippets.forEach(snippet => {
       document.getElementById('snippets').innerHTML += `
         <div class="snippet">
           <h3>${snippet.title}</h3>
-          <pre id="code-${index}">${snippet.code}</pre>
-          <button onclick="copyCode(${index})">📋 Copy</button>
+          <pre>${snippet.code}</pre>
         </div>
       `;
     });
   });
 
-function copyCode(id) {
-  let text = document.getElementById(`code-${id}`).innerText;
-  navigator.clipboard.writeText(text);
-  alert("Copied 🚀");
-}
+// leaderboard
+fetch('/data/contributors.json')
+  .then(res => res.json())
+  .then(data => {
+    let sorted = data.sort((a, b) => b.contributions - a.contributions);
+
+    sorted.forEach(user => {
+      document.getElementById('leaderboard').innerHTML += `
+        <li>${user.name} - ${user.contributions}</li>
+      `;
+    });
+  });
